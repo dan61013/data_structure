d54bbc9f0559d11bb5f75f862af8fd23c58368ff
dan.nb <max912131@gmail.com>
2024-11-07 15:35:55 +0800

    update Array example

--- Chapter01/1-1.py
+++ Chapter01/1-1.py
@@ -1,21 +1,25 @@
-import numpy as np
+# Practice: https://ithelp.ithome.com.tw/articles/10237739
 
 def main():
-    
-    house = np.array(['A','c','e','r','Y','F','R','z','G','a'])
-    
-    house[1], house[2], house[5], house[7], house[8] = '', '', '', '', ''
-    
-    house[1] = house[3]
-    house[3] = ""
-    
-    house[2] = house[6]
-    house[6] = ""
-    
-    house[3] = house[9]
-    house[9] = ""
-    
-    print(house)
-    
+    # 入住名單
+    arr = ["A", "c", "e", "r", "Y", "F", "R", "z", "G", "a"]
+
+    # 退房
+    arr[1] = ""
+    arr[2] = ""
+    arr[5] = ""
+    arr[7] = ""
+    arr[8] = ""
+
+    # 換房
+    arr[1] = arr[3]
+    arr[3] = ""
+    arr[2] = arr[6]
+    arr[6] = ""
+    arr[3] = arr[9]
+    arr[9] = ""
+
+    print(arr)
+
 if __name__ == "__main__":
     main()
